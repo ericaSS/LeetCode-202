47fddaa2f2cf87ca29eb4fb9335e3378746f5c77
ericaSS <38389389+ericaSS@users.noreply.github.com>
2018-06-13 07:49:52 -0400

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    Solution(){};
+    int isHappy(int n){
+        while(n){
+         n = sumOfDigits(n);
+         if(n==1) return true;
+         if(n==4) return false;
+        }
+    }
+    int sumOfDigits(int n){
+        int sum = 0;
+        while(n){
+            sum += pow(n%10, 2);
+            n = n/10;
+        }
+        return sum;
+    }
+};
+int main(){
+    Solution so;
+    cout << so.isHappy(19);
+}
